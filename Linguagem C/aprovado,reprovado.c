6642bfe488a52887598dca60895668871a1e0da4
0777rodrigo <128700789+0777rodrigo@users.noreply.github.com>
2023-05-23 11:06:21 -0300

    Add files via upload

---
+++ Linguagem C/aprovado,reprovado.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    float primeiraNota, segundaNota, media;
+
+
+    if (media >= 7) {
+        printf("aprovado");
+    }   else {
+        printf("reprovado");
+
+    }    
+
+        return 0;
+    }
